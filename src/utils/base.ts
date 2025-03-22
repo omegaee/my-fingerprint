585666ddb34375997e24cd667395253844d1c1f9
omegaee <2955281552@qq.com>
2025-03-22 20:12:16 +0800

    feat: Remove urlToHttpHost function

--- src/utils/base.ts
+++ src/utils/base.ts
@@ -55,30 +55,6 @@ export const arrayFilter = function <T>(arr: (T | undefined | boolean)[]): T[] {
   return arr.filter(item => item !== undefined && item !== false) as T[]
 }
 
-/**
- * url转带端口的host
- */
-export const urlToHttpHost = function (url: string) {
-  try {
-    let _url = new URL(url);
-    if (_url.protocol !== 'http:' && _url.protocol !== 'https:') {
-      return undefined
-    }
-    let hostname = _url.hostname
-    let port = _url.port
-    if (port === "") {
-      if (_url.protocol === "http:") {
-        port = "80";
-      } else if (_url.protocol === "https:") {
-        port = "443";
-      }
-    }
-    return `${hostname}:${port}`
-  } catch (err) {
-    return undefined
-  }
-}
-
 export const tryUrl = (url: string) => {
   try {
     return new URL(url);
