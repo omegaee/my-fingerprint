b9ca3ad7fbe692157d9825417442b2a613ba1433
omegaee <omegaee@github.com>
2025-07-28 14:26:41 +0800

    feat: update record UI

--- src/popup/record/item.tsx
+++ src/popup/record/item.tsx
@@ -4,6 +4,12 @@ import { useTranslation } from "react-i18next"
 
 const RootKeys = ['weak', 'strong', 'other']
 
+const KeyColors: Record<string, string> = {
+  strong: 'orange',
+  weak: 'green',
+  other: 'blue'
+}
+
 type FpNoticeItemProps = {
   title: string
   count?: number
@@ -20,8 +26,19 @@ export const FpNoticeItem = ({ title, count, isRoot }: FpNoticeItemProps) => {
     return title
   }, [isRoot, title, i18n.language])
 
-  return <div className="flex justify-between items-center font-bold">
-    <div>{label}</div>
-    <Tag bordered={false}>x{count}</Tag>
-  </div>
+  return isRoot ?
+    <div className="py-[2px] flex justify-between items-center font-bold">
+      <div>{label}</div>
+      <Tag color={KeyColors[title]}>
+        x {count}
+      </Tag>
+    </div>
+    :
+    <div className="flex justify-between items-center font-bold">
+      <div>{label}</div>
+      <Tag bordered={false}
+        color="transparent">
+        x {count}
+      </Tag>
+    </div>
 }
