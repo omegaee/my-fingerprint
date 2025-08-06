4367ac785b2760206b0e723ef21a699a21a9beb9
omegaee <omegaee@github.com>
2025-08-06 20:51:08 +0800

    Refactor popup config styles and structure

--- src/popup/config/index.tsx
+++ src/popup/config/index.tsx
@@ -1,4 +1,4 @@
-import { Collapse, theme, Typography, type CollapseProps } from "antd"
+import { Collapse, Typography, type CollapseProps } from "antd"
 import { useMemo } from "react"
 import { useTranslation } from "react-i18next"
 import WeakFpConfigGroup from "./group/weak"
@@ -9,47 +9,37 @@ import { usePrefsStore } from "../stores/prefs"
 
 export const FConfig = () => {
   const [t, i18n] = useTranslation()
-  const { token } = theme.useToken();
 
   const prefs = usePrefsStore()
 
   const items = useMemo<CollapseProps['items']>(() => {
-    const style: React.CSSProperties = {
-      marginBottom: 8,
-      // background: token.colorFillContent,
-      borderRadius: token.borderRadiusSM,
-      border: '2px solid',
-      borderColor: token.colorBorder,
-    }
     return [
       {
         label: <Typography.Text className="font-bold">{t('label.config.strong')}</Typography.Text>,
         children: <StrongFpConfigGroup />,
-        style,
       },
       {
         label: <Typography.Text className="font-bold">{t('label.config.weak')}</Typography.Text>,
         children: <WeakFpConfigGroup />,
-        style,
       },
       {
         label: <Typography.Text className="font-bold">{t('label.config.script')}</Typography.Text>,
         children: <ScriptConfigGroup />,
-        style,
       },
       {
         label: <Typography.Text className="font-bold">{t('label.config.prefs')}</Typography.Text>,
         children: <PrefsConfigGroup />,
-        style,
       },
-    ].map((item, key) => ({ ...item, key }))
+    ].map((item, key) => ({
+      ...item,
+      key,
+      className: 'mb-2 last:mb-0 py-1 !border-0 bg-[--ant-color-bg-container] !rounded-md',
+    })) as CollapseProps['items']
   }, [i18n.language, prefs.theme])
 
-  return <Collapse className="h-full" size='small'
-    style={{
-      background: 'transparent',
-    }}
-    expandIconPosition='end'
+  return <Collapse
+    size='small'
+    className="h-full bg-transparent overflow-auto no-scrollbar"
     bordered={false}
     accordion
     items={items} />
